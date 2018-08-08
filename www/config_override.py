80dfc60a9c9ed6669a8b7fb974e9a58ab3e9031c
郭黎辉 <guolihui@guolihuideMBP.lan>
2018-08-08 15:07:28 +0800

    python3-demo

---
+++ www/config_override.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+'''
+Override configurations.
+'''
+
+__author__ = 'Michael Guo'
+
+configs = {
+    'db': {
+        'host': '127.0.0.1'
+    }
+}
